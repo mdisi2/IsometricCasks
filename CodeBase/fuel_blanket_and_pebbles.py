ddcdf34fd18d155f72a7361ce3ecd5f6cc24793a
mdisi2 <mdisi2@illinois.edu>
2026-02-03 08:27:04 -0600

    work

--- CodeBase/fuel_blanket_and_pebbles.py
+++ CodeBase/fuel_blanket_and_pebbles.py
@@ -1,7 +1,6 @@
 import openmc 
 
 from Function_Folder.mats import S_316_borated, Concrete
-from Function_Folder.utilities import extrude
 
 ### This file constructs the cell for the fuel blanket and pebbles, with reflective boundary conditions to fill the mpc universe
 ### In units [cm]
@@ -17,20 +16,9 @@ y_2 = openmc.YPlane(y0 =  6.25 / 2,boundary_type='reflective')
 
 Boundary_Region = +z_bottom & -z_top & +x_1 & -x_2 & +y_1 & -y_2
 
-#Bounding Box for xz quad 1 
-x_0 = openmc.XPlane(x0=0,boundary_type='reflective')
-x_f = openmc.XPlane(x0=3.125,boundary_type='reflective')
-z_t = openmc.ZPlane(z0=5.503,boundary_type='reflective')
-z_b = openmc.ZPlane(z0=0,boundary_type='reflective')
-y_0 = openmc.YPlane(y0=-3.125,boundary_type='reflective')
-y_f = openmc.YPlane(y0=3.125,boundary_type='reflective')
-
-
-bounding_box = +x_0 & -x_f & +z_b & -z_t & +y_0 & -y_f
-
 def Fuel_Blanket():
 
-    ## XZ plane 
+    ## Main frame
     PXZ_1 = [(3.025,0),
              (3.125,0),
              (3.125,1.804),
@@ -40,22 +28,107 @@ def Fuel_Blanket():
              (0,3.493),
              (3.025,1.746)]
     
+    PXZ_2 = [(-3.025,0),
+             (-3.125,0),
+             (-3.125,1.804),
+             (-0.1,3.551),
+             (-0.1,5.503),
+             (-0,5.503),
+             (-0,3.493),
+             (-3.025,1.746)]
+    
+    PXZ_3 = [(-3.025,-0),
+             (-3.125,-0),
+             (-3.125,-1.804),
+             (-0.1,-3.551),
+             (-0.1,-5.503),
+             (-0,-5.503),
+             (-0,-3.493),
+             (-3.025,-1.746)]
+    
+    PXZ_4 = [(3.025,-0),
+             (3.125,-0),
+             (3.125,-1.804),
+             (0.1,-3.551),
+             (0.1,-5.503),
+             (0,-5.503),
+             (0,-3.493),
+             (3.025,-1.746)]
+    
+
+    frame_outer = [(0.1,5.503),
+                (-0.1,5.503),
+                (-0.1,3.551),
+                (-3.125,1.804),
+                (-3.125,-1.804),
+                (-0.1,-3.551),
+                (-0.1,-5.503),
+                (0.1,-5.503),
+                (0.1,-3.551),
+                (3.125,-1.804),
+                (3.125,1.804),
+                (0.1,3.551)]
+    
+    frame_cut = [(0,3.493),
+                 (-3.025, 1.746),
+                 (-3.025,-1.746),
+                 (0,-3.493),
+                 (3.025,-1.746),
+                 (3.025,1.764)]
+    
+    ### Center cuts
+    
     PXZ_1_hole = [(0,0),
                   (1.732,0),
                   (0.866,1.5),
                   (0,1.5)]
+    
+    PXZ_2_hole = [(0,0),
+                  (-1.732,0),
+                  (-0.866,1.5),
+                  (-0,1.5)]
+    
+    PXZ_2_hole = [(0,0),
+                  (-1.732,0),
+                  (-0.866,-1.5),
+                  (-0,-1.5)]
+    
+    PXZ_2_hole = [(0,0),
+                  (1.732,0),
+                  (0.866,-1.5),
+                  (0,-1.5)]
+    
+    ### Corner cuts
     
     PXZ_1_hole2 = [(3.125,4.003),
                   (3.125,5.503),
                   (1.393,5.503),
                   (2.259,4.003)]
     
+    PXZ_2_hole2 = [(-3.125,4.003),
+                  (-3.125,5.503),
+                  (-1.393,5.503),
+                  (-2.259,4.003)]
+    
+    PXZ_3_hole2 = [(-3.125,-4.003),
+                  (-3.125,-5.503),
+                  (-1.393,-5.503),
+                  (-2.259,-4.003)]
+    
+    PXZ_3_hole2 = [(3.125,-4.003),
+                  (3.125,-5.503),
+                  (1.393,-5.503),
+                  (2.259,-4.003)]
+    
 
     Region_1 = openmc.model.Polygon(points=PXZ_1, basis='xz').region
     Region_cut_1 = openmc.model.Polygon(points=PXZ_1_hole, basis='xz').region
+
+
+    
     Region_cut_2 = openmc.model.Polygon(points=PXZ_1_hole2, basis='xz').region
 
-    total_region = (Region_1 & ~Region_cut_1 & ~Region_cut_2) & bounding_box
+    total_region = (Region_1 & ~Region_cut_1 & ~Region_cut_2) & Boundary_Region
 
 
     F_B = openmc.Cell(name='Fuel Blanket',
@@ -65,16 +138,64 @@ def Fuel_Blanket():
     return F_B
 
 
+def basket():
+
+    frame_outer = [(0.1,5.503),
+                (-0.1,5.503),
+                (-0.1,3.551),
+                (-3.125,1.804),
+                (-3.125,-1.804),
+                (-0.1,-3.551),
+                (-0.1,-5.503),
+                (0.1,-5.503),
+                (0.1,-3.551),
+                (3.125,-1.804),
+                (3.125,1.804),
+                (0.1,3.551)]
+    
+    frame_cut = [(0,3.493),
+                 (3.025, 1.746),
+                 (3.025,-1.746),
+                 (0,-3.493),
+                 (-3.025,-1.746),
+                 (-3.025,1.764)]
+
+    frame_xz = openmc.model.Polygon(points=frame_outer,basis='xz')
+    cut_xz = openmc.model.Polygon(points=frame_cut,basis='xz')
+    frame_region_xy = +cut_xz.region & -frame_xz.region
+
+
+
+
+    frame = openmc.Cell(name='blanket',
+                        region = frame_region_xy,
+                        fill = Concrete ) #frame_region_yz
+    
+    return frame
+
+
+
 def Triso_Pebble():
     #sphere in xy plane
 
     Centered = openmc.Sphere(x0=0, y0=0, z0=0 , r =3.0)
     Top_far = openmc.Sphere(x0=3.125, y0=3.125, z0= 5.503, r =3.0)
     Top_close = openmc.Sphere(x0=3.125, y0=-3.125, z0= 5.503, r =3.0)
-    region = -Centered | -Top_far | -Top_close
+    
+    t_1 = openmc.Sphere(x0=3.125, y0=3.125, z0= 5.503, r =3.0)
+    t_2 = openmc.Sphere(x0=-3.125, y0=3.125, z0= 5.503, r =3.0)
+    t_3 = openmc.Sphere(x0=-3.125, y0=-3.125, z0= 5.503, r =3.0)
+    t_4 = openmc.Sphere(x0=3.125, y0=-3.125, z0= 5.503, r =3.0)
+
+    b_1 = openmc.Sphere(x0=3.125, y0=3.125, z0= -5.503, r =3.0)
+    b_2 = openmc.Sphere(x0=-3.125, y0=3.125, z0= -5.503, r =3.0)
+    b_3 = openmc.Sphere(x0=-3.125, y0=-3.125, z0= -5.503, r =3.0)
+    b_4 = openmc.Sphere(x0=3.125, y0=-3.125, z0= -5.503, r =3.0)
+    
+    region = -Centered | -t_1 | -t_2 | -t_3 | -t_4 | -b_1 | -b_2 | -b_3 | -b_4
 
     Triso_Pebble = openmc.Cell(name='Pebbles',
-                               region=region & bounding_box,
+                               region=region & Boundary_Region,
                                fill=Concrete) # TODO triso pebble fill
 
     return Triso_Pebble

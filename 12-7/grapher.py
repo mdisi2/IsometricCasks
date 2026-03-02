8d647dd626e2718a45dec2a150b163282fbbaa35
Matthew Disimone <mdisi2@illinois.edu>
2026-03-02 08:29:24 -0600

    stretched pics

--- 12-7/grapher.py
+++ 12-7/grapher.py
@@ -2,7 +2,7 @@ import openmc
 import matplotlib.pyplot as plt
 import numpy as np
 
-path = 'Pictures/statepoint.100.h5'
+path = '12-7/statepoint.100boratedwater.h5'
 
 sp = openmc.StatePoint(path)
 t = sp.get_tally(name='neutron_spectrum')
@@ -22,16 +22,16 @@ dlnE  = np.log(E_bins[1:] / E_bins[:-1])
 flux_lethargy = flux / dlnE
 flux_lethargy /= np.sum(flux_lethargy * dlnE)
 
-plt.figure()
+plt.figure(figsize=(12,7))
 plt.loglog(E_mid, flux, color='black')
 plt.xlabel("Energy [eV]")
 plt.ylabel("Normalized Flux / lethargy")
 plt.xscale('log')
 plt.yscale('log')
-plt.title("Neutron Spectrum Normal Conditions")
+plt.title("Neutron Spectrum | Water Submersion (HAC) | Borated Blanket")
 plt.grid(True, alpha=0.5)
 plt.axvline(1,linestyle='--',color='red')
 plt.axvline(10e3,linestyle='--',color='red')
 plt.tight_layout()
-plt.savefig('flux_spectrum.png', dpi=600)
+plt.savefig('flux_spectrum_HAC_B.png', dpi=600)
 plt.show()
